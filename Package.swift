55603c326a8c0ee34d2daa38b00b66d263b17c33
Sridatta <snallamilli@efficiencyware.com>
2024-08-16 20:05:06 +0530

    Initial commit with swift package project files

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PR1DigitalAdClassic",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PR1DigitalAdClassic",
+            targets: ["PR1DigitalAdClassic"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "PR1DigitalAdClassic",
+            path: "Sources/PR1DigitalAdClassic_0.0.7.xcframework"
+        )
+    ]
+)
